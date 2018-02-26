127a00a47d41c3aac446800162d72eafc401a493
Ollie Thomas <ollie.thomas1992@gmail.com>
2018-02-26 23:41:30 +0000

    moved sidebar logic

--- src/client/global/components/Header.js
+++ src/client/global/components/Header.js
@@ -5,54 +5,23 @@ import Sidebar from "react-sidebar";
 import SideBarContent from "./Sidebar";
 // import menu from '../../assets/icons/menu.svg';
 
-export default class Header extends Component {
-    constructor(props) {
-        super(props);
+export default ({ onSetSidebarOpen }) => {
+    return (
+        <nav className="flex items-center justify-between flex-wrap bg-white p-6">
+            {/* Purpe Circle */}
+            <div className="flex items-center flex-no-shrink text-white mr-6">
+                <div className="rounded-full h-12 w-12 flex items-center justify-center bg-purple" />
+            </div>
 
-        this.state = {
-            sidebarOpen: true
-        };
-
-        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
-    }
-
-    onSetSidebarOpen(open) {
-        this.setState({ sidebarOpen: open });
-    }
-
-    render() {
-        return (
-            <nav className="flex items-center justify-between flex-wrap bg-white p-6">
-                {/* Purpe Circle */}
-                <div className="flex items-center flex-no-shrink text-white mr-6">
-                    <div className="rounded-full h-12 w-12 flex items-center justify-center bg-purple" />
-                </div>
-
-                {/* Menu */}
-                <div className="block">
-                    <button
-                        onClick={() => onSetSidebarOpen(true)}
-                        className="flex items-center px-3 py-2 text-purple"
-                    >
-                    Open
-                    </button>
-                </div>
-
-                <Sidebar
-                    sidebar={
-                        <SideBarContent
-                            onSetSidebarOpen={this.onSetSidebarOpen}
-                        />
-                    }
-                    shadow={false}
-                    pullRight={true}
-                    open={this.state.sidebarOpen}
-                    onSetOpen={this.onSetSidebarOpen}
-                    sidebarClassName={"w-full"}
+            {/* Menu */}
+            <div className="block">
+                <button
+                    onClick={() => onSetSidebarOpen(true)}
+                    className="flex items-center px-3 py-2 text-purple"
                 >
-                    <b>hello</b>
-                </Sidebar>
-            </nav>
-        );
-    }
-}
+                    Open
+                </button>
+            </div>
+        </nav>
+    );
+};
--- src/client/global/containers/App.js
+++ src/client/global/containers/App.js
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import { renderRoutes } from "react-router-config";
 import Sidebar from "react-sidebar";
 
 import Header from "../components/Header";
+import SideBarContent from "../components/Sidebar";
 
-const App = ({ route }) => {
-    return (
-        <div>
-            <Header />
-            {renderRoutes(route.routes)}
-        </div>
-    );
-};
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            sidebarOpen: true
+        };
+
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+    }
+
+    onSetSidebarOpen(open) {
+        this.setState({ sidebarOpen: open });
+    }
+
+    render() {
+        return (
+            <div>
+                <Sidebar
+                    sidebar={
+                        <SideBarContent
+                            onSetSidebarOpen={this.onSetSidebarOpen}
+                        />
+                    }
+                    shadow={false}
+                    pullRight={true}
+                    open={this.state.sidebarOpen}
+                    onSetOpen={this.onSetSidebarOpen}
+                    sidebarClassName={"w-full"}
+                >
+                    <Header onSetSidebarOpen={this.onSetSidebarOpen} />
+                    {renderRoutes(this.props.route.routes)}
+                </Sidebar>
+            </div>
+        );
+    }
+}
 
 export default {
     component: App
