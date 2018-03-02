4bee305f9a244dec0d2c12acbd0749c9f509078e
Ollie Thomas <ollie.thomas1992@gmail.com>
2018-03-02 00:19:54 +0000

    remove frontend from watch

--- src/client/pages/About.js
+++ src/client/pages/About.js
@@ -1,24 +1,24 @@
 import React from "react";
 import Layout from "../global/components/Layout";
 
+import FeifaLogo from '../assets/images/feifa-logo.png'
+
 const About = () => (
     <Layout>
         <h1 className="h1">About</h1>
-        <div className="something">
-            <p className="my-4">
-                For international firms with global focus looking at new or
-                existing jurisdictions to conduct business, service existing
-                clients or just needing a fresh approach.
-            </p>
+        <div className="flex">
+            <div className="w-3/4">
+                <p className="my-4">
+                    For international firms with global focus looking at new or&#10;existing jurisdictions to conduct business, service existing&#10;clients or just needing a fresh approach.
+                </p>
 
-            <p className="my-4">
-                Innovate LSO oãers a wealth of experience via an extensive panel
-                of preferred suppliers supervised by an internationally
-                experienced management team. A global outreach on your doorstep.
-            </p>
-        </div>
-        <div>
-            
+                <p className="my-4">
+                    Innovate LSO o&atilde;ers a wealth of experience via an extensive panel&#10;of preferred suppliers supervised by an internationally&#10;experienced management team. A global outreach on your doorstep.
+                </p>
+            </div>
+            <div className="1/4">
+                <img src={FeifaLogo} className="w-1/5" />
+            </div>
         </div>
     </Layout>
 );
