4129475d3dfb6e3e5f8f8dd6343304c4d48bbd88
Ollie Thomas <ollie.thomas1992@gmail.com>
2018-03-04 13:36:07 +0000

    removed cssnano

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,13 +1,12 @@
 var isDev = require("isDev");
 var tailwindcss = require("tailwindcss");
 
-const cssnano = (!isDev) ? require("cssnano") : '';
+// const cssnano = (!isDev) ? require("cssnano") : '';
 
 module.exports = {
     plugins: [
         tailwindcss("./config/tailwind.config.js"),
         require("precss"),
-        cssnano,
         require("postcss-cssnext")
     ]
 };
