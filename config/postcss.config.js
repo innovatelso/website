0b7e2107ba485908a7e8330ff63c1bbf346f61c4
Ollie Thomas <ollie.thomas1992@gmail.com>
2018-03-04 12:39:03 +0000

    changed logic in postcss confgi

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,11 +1,13 @@
 var isDev = require("isDev");
 var tailwindcss = require("tailwindcss");
 
+const cssnano = (!isDev) ? require("cssnano") : '';
+
 module.exports = {
     plugins: [
         tailwindcss("./config/tailwind.config.js"),
         require("precss"),
-        require("postcss-cssnext"),
-        (!isDev) ? require("cssnano") : ''
+        cssnano,
+        require("postcss-cssnext")
     ]
 };
