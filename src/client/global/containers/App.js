ff8e4c4d490bb42146f7e19cb83fd2fd73d577c7
Ollie Thomas <ollie.thomas1992@gmail.com>
2018-03-11 11:29:04 +0000

    added bold fonts and sidebar default open state is now false

--- src/client/global/containers/App.js
+++ src/client/global/containers/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         super(props)
 
         this.state = {
-            isSidebarOpen: true
+            isSidebarOpen: false
         }
 
         this.setSidebarOpenState = this.setSidebarOpenState.bind(this);
--- src/client/pages/About.js
+++ src/client/pages/About.js
@@ -7,7 +7,7 @@ const About = () => (
     <Layout>
         <div className="flex">
             <div className="flex-1">
-                <h1 className="h1">About</h1>
+                <h1 className="h1 font-bold">About</h1>
                 <div className="flex-2">
                     <p className="my-4">
                         For international firms with global focus looking at new or&#10;existing jurisdictions to conduct business, service existing&#10;clients or just needing a fresh approach.
